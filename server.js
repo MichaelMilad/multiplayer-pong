7d235b1516974199b82a68e3304b6ffb90b8fdbc
Michael Milad <michaelmilad001@gmail.com>
2025-07-14 20:40:35 +0300

    fini

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const http = require('http');
 const io = require('socket.io');
 
 const apiServer = require('./api');
+const sockets = require('./sockets');
+
 const httpServer = http.createServer(apiServer);
 const socketServer = io(httpServer);
 
-const sockets = require('./sockets');
-
 const PORT = 3000;
 httpServer.listen(PORT);
-console.log(`Listening on port ${PORT}...`);
-
 sockets.listen(socketServer);
+console.log(`Listening on port ${PORT}...`);
